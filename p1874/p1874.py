d7d6472ed93e92cf40493128fa575db4b5d57b77
donghyunlee <edgus1234@gmail.com>
2018-07-27 15:00:35 +0900

    why??

--- p1874/p1874.py
+++ p1874/p1874.py
@@ -1,17 +1,51 @@
 import sys
 
+def find_input(num):
+    # True if in stack , flase in number
+    if num in stack:
+        return True
+    else:
+        return False
 
 num_max = int(sys.stdin.readline().rstrip())
 numbers = list(range(1,num_max + 1))
+stack = []
+stack_top = 0
+cmd = ''
 ind = num_max
+exception = False
 while ind:
-
+    print(numbers)
+    print(stack)
     _input = int(sys.stdin.readline().rstrip())
 
-    # check already pop
-    if not _input in numbers:
-        sys.stdout.write('NO\n')
-        break
+    if find_input(_input):
+        if _input > stack_top:
+            #error
+            exception = True
+            pass
+        elif _input == stack_top:
+            cmd += '-'
+            stack.remove(_input)
+            stack_top = stack[-1]
+        elif _input < stack_top:
+            for elem in stack:
+                if elem >= _input:
+                    stack.remove(elem)
+                    stack_top = stack[-1]
+                    cmd += '-'
 
-    numbers.remove(_input)
+    else:
+        for elem in numbers:
+            print(elem)
+            if elem < _input:
+                stack.append(elem)
+                numbers.remove(elem)
+                stack_top = stack[-1]
+                cmd += '+'
+            elif elem == _input:
+                numbers.remove(elem)
+                cmd += '+-'
     ind = ind - 1
+
+sys.stdout.write(cmd+'\n' + str(exception))
